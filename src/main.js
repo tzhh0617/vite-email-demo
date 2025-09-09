d243be9033fe1c034a36ca152a6caaea92d27804
Chris Tian <chris.tian@biposervice.com>
2025-09-09 15:12:21 +0800

    chore

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from './router/index.js'
+import './style.css'
 
 const app = createApp(App)
 app.use(ElementPlus)
