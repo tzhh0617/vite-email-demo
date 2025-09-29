337938bf7723b4524ec17f4ebf8ee52951cecda4
Chris Tian <chris.tian@biposervice.com>
2025-09-29 14:43:00 +0800

    refactor(TinyMCEDemo): 重构 TinyMCE 编辑器实现方式
    将基于 @tinymce/tinymce-vue 组件的实现改为直接使用 tinymce API
    移除对 @tinymce/tinymce-vue 的依赖，改为手动初始化和销毁编辑器实例
    添加编辑器内容变化监听和手动设置内容逻辑

--- src/pages/solution-tinymce-2/TinyMCEDemo.vue
+++ src/pages/solution-tinymce-2/TinyMCEDemo.vue
@@ -1,26 +1,9 @@
 <template>
   <div class="page">
     <h2>TinyMCE 富文本编辑器 inline</h2>
-    <Editor
-      v-model="content"
-      :init="{
-        height: 500,
-        menubar: false,
-        inline: true,
-        plugins:
-          'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
-        toolbar:
-          'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
-        content_style: `
-        .editable { border: 1px solid transparent; }
-        .editable:focus,.editable:hover { border-color: green; }
-        `,
-        editable_root: false,
-        editable_class: 'editable',
-      }"
-      :tinymceScriptSrc="'/node_modules/tinymce/tinymce.min.js'"
-      licenseKey="gpl"
-    />
+    <div ref="editorRef" class="tinymce-container">
+      <p>欢迎使用 TinyMCE 富文本编辑器！</p>
+    </div>
     <el-button type="primary" style="margin: 16px 0" @click="setFilledContent">
       填充邮件内容
     </el-button>
@@ -36,15 +19,69 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-import Editor from "@tinymce/tinymce-vue";
+import { ref, onMounted, onUnmounted } from "vue";
+import tinymce from "tinymce/tinymce";
+import "tinymce/themes/silver";
+import "tinymce/icons/default";
+import 'tinymce/models/dom'
+import "tinymce/plugins/anchor";
+import "tinymce/plugins/autolink";
+import "tinymce/plugins/charmap";
+import "tinymce/plugins/codesample";
+import "tinymce/plugins/image";
+import "tinymce/plugins/link";
+import "tinymce/plugins/lists";
+import "tinymce/plugins/media";
+import "tinymce/plugins/searchreplace";
+import "tinymce/plugins/table";
+import "tinymce/plugins/visualblocks";
+import "tinymce/plugins/wordcount";
 import { ElButton } from "element-plus";
 import { EMAIL_TEMPLATE_FILLED } from "@/constants/email.js";
 
 const content = ref("<p>欢迎使用 TinyMCE 富文本编辑器！</p>");
+const editorRef = ref(null);
+let editor = null;
+
+onMounted(() => {
+  initTinyMCE();
+});
+
+onUnmounted(() => {
+  if (editor) {
+    editor.destroy();
+  }
+});
+
+function initTinyMCE() {
+  tinymce.init({
+    target: editorRef.value,
+    height: 500,
+    menubar: false,
+    inline: true,
+    plugins: 'anchor autolink charmap codesample image link lists media searchreplace table visualblocks wordcount',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | charmap | removeformat',
+    content_style: `
+      .editable { border: 1px solid transparent; }
+      .editable:focus,.editable:hover { border-color: green; }
+    `,
+    // editable_root: false,
+    // editable_class: 'editable',
+    license_key: 'gpl',
+    setup: (ed) => {
+      editor = ed;
+      ed.on('input change', () => {
+        content.value = ed.getContent();
+      });
+    }
+  });
+}
 
 function setFilledContent() {
   content.value = EMAIL_TEMPLATE_FILLED;
+  if (editor) {
+    editor.setContent(EMAIL_TEMPLATE_FILLED);
+  }
 }
 </script>
 
